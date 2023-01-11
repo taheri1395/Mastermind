7f37081b2fe040129489ed4e05a54590a747bec5
محمد نوروز زادگان <norouzzadegan@Mahsan.co>
2023-01-11 13:51:05 +0330

    Update test models with TDD

--- src/mastermind/domain/models.py
+++ src/mastermind/domain/models.py
@@ -1,27 +1,42 @@
 from __future__ import annotations
 
-from typing import Sequence
+from typing import Final, Sequence
 from enum import Enum
 
 
 class Mastermind:
+    MAXIMUM_NUMBER_OF_GUESSES: Final[int] = 10
+    NUMBER_OF_CODE_PEGS: Final[int] = 4
+
+    def __init__(self, code_maker: CodeMaker):
+        self._trials = []
+        self._code_maker = code_maker
+
     @property
     def trials(self) -> Sequence:
-        return []
+        return self._trials
 
     @property
     def state(self) -> str:
+        if len(self._trials) >= self.MAXIMUM_NUMBER_OF_GUESSES:
+            return 'lost'
         return 'playing'
 
-    def guess(self, colors: Sequence[Color]) -> None:
-        if len(colors) != 4:
-            raise ValueError('Invalid number of colors.')
+    def guess(self, code_pegs: Sequence[CodePeg]) -> None:
+        if len(code_pegs) != self.NUMBER_OF_CODE_PEGS:
+            raise ValueError('Invalid number of code-pegs.')
+        self._trials.append(code_pegs)
+        
 
 
-class Color(str, Enum):
+class CodePeg(str, Enum):
     RED = 'red'
     BLUE = 'blue'
     GREEN = 'green'
     YELLOW = 'yellow'
     WHITE = 'white'
     BLACK = 'black'
+
+
+class CodeMaker:
+    pass
--- tests/domain/test_models.py
+++ tests/domain/test_models.py
@@ -1,21 +1,22 @@
-from mastermind.domain.models import Mastermind, Color
+from mastermind.domain.models import Mastermind, CodePeg
 from typing import Sequence
 from pytest import raises
 import re
+from unittest.mock import Mock
 
 
 def test_starting_a_new_game() -> None:
     # Arrange (empty)
 
     # Act
-    game = Mastermind()
+    game = Mastermind(code_maker=Mock())
 
     # Assert (empty)
 
 
 def test_getting_trials_of_a_new_game() -> None:
     # Arrange
-    game = Mastermind()
+    game = Mastermind(code_maker=Mock())
 
     # Act
     trials = game.trials
@@ -27,7 +28,7 @@ def test_getting_trials_of_a_new_game() -> None:
 
 def test_state_of_a_new_game() -> None:
     # Arrange
-    game = Mastermind()
+    game = Mastermind(code_maker=Mock())
 
     # Act
     state = game.state
@@ -38,27 +39,52 @@ def test_state_of_a_new_game() -> None:
 
 def test_making_a_guess_on_a_new_game() -> None:
     # Arrange
-    game = Mastermind()
+    game = Mastermind(code_maker=Mock())
 
     # Act
-    game.guess([Color.RED] * 4)
+    game.guess([CodePeg.RED] * Mastermind.NUMBER_OF_CODE_PEGS)
 
     # Assert (empty)
 
 
-def test_making_a_guess_with_more_than_sufficient_number_of_colors() -> None:
+def test_making_a_guess_with_more_than_sufficient_number_of_code_pegs() -> None:
     # Arrange
-    game = Mastermind()
+    game = Mastermind(code_maker=Mock())
 
     # Act, Assert
-    with raises(ValueError, match='number of colors'):
-        game.guess([Color.RED] * 5)
+    with raises(ValueError, match='number of code-pegs'):
+        game.guess([CodePeg.RED] * (Mastermind.MAXIMUM_NUMBER_OF_GUESSES + 1))
 
 
-def test_making_a_guess_with_less_than_sufficient_number_of_colors() -> None:
+def test_making_a_guess_with_less_than_sufficient_number_of_code_pegs() -> None:
     # Arrange
-    game = Mastermind()
+    game = Mastermind(code_maker=Mock())
 
     # Act, Assert
-    with raises(ValueError, match='number of colors'):
-        game.guess([Color.RED] * 3)
+    with raises(ValueError, match='number of code-pegs'):
+        game.guess([CodePeg.RED] * (Mastermind.MAXIMUM_NUMBER_OF_GUESSES - 1))
+
+
+def test_making_a_guess_should_add_a_new_trial() -> None:
+    # Arrange
+    game = Mastermind(code_maker=Mock())
+
+    # Act
+    game.guess([CodePeg.RED] * Mastermind.NUMBER_OF_CODE_PEGS)
+
+    # Assert
+    assert len(game.trials) == 1
+
+
+def test_state_after_maximum_number_of_wrong_guesses() -> None:
+    # Arrange
+    code_maker = Mock(make=Mock(return_value=[CodePeg.BLUE] * Mastermind.NUMBER_OF_CODE_PEGS))
+    game = Mastermind(code_maker=code_maker)
+    for i in range(Mastermind.MAXIMUM_NUMBER_OF_GUESSES):
+        game.guess([CodePeg.RED] * Mastermind.NUMBER_OF_CODE_PEGS)
+
+    # Act
+    state = game.state
+    
+    # Assert
+    assert state == "lost"
