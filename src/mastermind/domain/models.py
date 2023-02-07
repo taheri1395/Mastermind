451f6e11b75ad03571120ac2a16ec589a3c6e81f
taheri1395 <84721594+taheri1395@users.noreply.github.com>
2023-02-07 17:03:05 +0330

    Update models.py

--- src/mastermind/domain/models.py
+++ src/mastermind/domain/models.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from typing import Final, Sequence
+from typing import Final, Sequence, List, Iterable
 from enum import Enum
+from dataclasses import dataclass
 
 
 class Mastermind:
@@ -9,24 +10,54 @@ class Mastermind:
     NUMBER_OF_CODE_PEGS: Final[int] = 4
 
     def __init__(self, code_maker: CodeMaker):
-        self._trials = []
-        self._code_maker = code_maker
+        self._trials: List[Trial] = []
+        self._secret_pegs = code_maker.make()
 
     @property
-    def trials(self) -> Sequence:
+    def secret_pegs(self) -> Sequence[CodePeg]:
+        if self.state == 'playing':
+            raise AssertionError('Secret pegs are only visible when game is finished.')
+        return self._secret_pegs
+
+    @property
+    def trials(self) -> Sequence[Trial]:
         return self._trials
 
     @property
     def state(self) -> str:
         if len(self._trials) >= self.MAXIMUM_NUMBER_OF_GUESSES:
             return 'lost'
+        if self._trials and self._trials[-1].guess_pegs == self._secret_pegs:
+            return 'won'
         return 'playing'
 
-    def guess(self, code_pegs: Sequence[CodePeg]) -> None:
-        if len(code_pegs) != self.NUMBER_OF_CODE_PEGS:
+    def guess(self, guess_pegs: Sequence[CodePeg]) -> None:
+        if self.state in ('lost', 'won'):
+            raise AssertionError(
+                'A new guess is not allowed when the state of the game is '
+                '"{}".'.format(self.state)
+            )
+        if len(guess_pegs) != self.NUMBER_OF_CODE_PEGS:
             raise ValueError('Invalid number of code-pegs.')
-        self._trials.append(code_pegs)
-        
+        self._trials.append(Trial(guess_pegs, self._compare(guess_pegs)))
+
+    def _compare(self, guess_pegs: Sequence[CodePeg]) -> Iterable[KeyPeg]:
+        result = []
+        checked_indices = []
+        for i, secret_peg in enumerate(self._secret_pegs):
+            if secret_peg == guess_pegs[i] and i not in checked_indices:
+                result.append(KeyPeg.WHITE)
+                checked_indices.append(i)
+        for i, secret_peg in enumerate(self._secret_pegs):
+            if secret_peg in guess_pegs and i not in checked_indices:
+                result.append(KeyPeg.BLACK)
+                checked_indices.append(i)
+        return result
+
+
+class CodeMaker:
+    def make(self) -> Sequence[CodePeg]:
+        pass
 
 
 class CodePeg(str, Enum):
@@ -34,9 +65,16 @@ class CodePeg(str, Enum):
     BLUE = 'blue'
     GREEN = 'green'
     YELLOW = 'yellow'
+    PINK = 'pink'
+    BROWN = 'brown'
+
+
+class KeyPeg(str, Enum):
     WHITE = 'white'
     BLACK = 'black'
 
 
-class CodeMaker:
-    pass
+@dataclass
+class Trial:
+    guess_pegs: Sequence[CodePeg]
+    response_pegs: Iterable[KeyPeg]
