207fb58bda195811a9b15ac2a7bb8eb88a4ef7aa
محمد نوروز زادگان <norouzzadegan@Mahsan.co>
2023-02-22 15:08:45 +0330

    Add GUI python.

--- tests/domain/test_models.py
+++ tests/domain/test_models.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from mastermind.domain.models import Mastermind, CodePeg, KeyPeg, CodeMaker
-=======
-from mastermind.domain.models import Mastermind, CodePeg, KeyPeg
->>>>>>> de77e00287c9505ff241c88b5efc631ae7462228
 from typing import Sequence
 from pytest import raises
 import re
