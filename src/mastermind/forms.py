809e7feef45355f5d6b220adb633fad40c19a244
محمد نوروز زادگان <norouzzadegan@Mahsan.co>
2023-05-24 15:02:22 +0430

    Update Django template.

---
+++ src/mastermind/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+BIRTH_YEAR_CHOICES = ["1980", "1981", "1982"]
+FAVORITE_COLORS_CHOICES = [
+    ("blue", "Blue"),
+    ("green", "Green"),
+    ("black", "Black"),
+]
+
+
+class SimpleForm(forms.Form):
+    birth_year = forms.DateField(
+        widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES)
+    )
+    favorite_colors = forms.ChoiceField(
+        required=False,
+        choices=FAVORITE_COLORS_CHOICES,
+    )
--- src/mastermind/views.py
+++ src/mastermind/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from .forms import SimpleForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == 'POST':
+        form = SimpleForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['favorite_colors'])
+    return render(request, "index.jinja2", {"form": SimpleForm()})
+
+def submit(request):
+    pass
